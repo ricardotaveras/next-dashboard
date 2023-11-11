0fb2ef46eb858044e5bfc7a12dea907dfc5c8af6
Ricard Ttaveras <ricardo.taveras@gmail.com>
2023-11-11 13:42:08 -0400

    Fix Update invoice problem cache

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -4,6 +4,7 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { unstable_noStore as noStore } from 'next/cache';
 
 const InvoiceSchema = z.object({
 	id: z.string(),
@@ -14,8 +15,11 @@ const InvoiceSchema = z.object({
 });
 
 const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
+const UpdateInvoice = InvoiceSchema.omit({ date: true, id: true });
 
 export async function createInvoice(formData: FormData) {
+	noStore();
+
 	// const rawFormData = {
 	// 	customerId: formData.get('customerId'),
 	// 	amount: formData.get('amount'),
@@ -35,7 +39,26 @@ export async function createInvoice(formData: FormData) {
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
 	// Test it out:
-	console.log('RawFormData', formData);
+	// console.log('RawFormData', formData);
+	revalidatePath('/dashboard/invoices');
+	redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+	noStore();
+	const { customerId, amount, status } = UpdateInvoice.parse({
+		customerId: formData.get('customerId'),
+		amount: formData.get('amount'),
+		status: formData.get('status'),
+	});
+
+	const amountInCents = amount * 100;
+	// console.log('customerId: ', customerId);
+	await sql<any>`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
 	revalidatePath('/dashboard/invoices');
 	redirect('/dashboard/invoices');
 }

01ba2c12a38be518e4d43cc21ae83b205ad32a10
Ricard Ttaveras <ricardo.taveras@gmail.com>
2023-11-12 13:11:25 -0400

    Add login

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -5,6 +5,7 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { unstable_noStore as noStore } from 'next/cache';
+import { signIn } from '@/auth';
 
 const InvoiceSchema = z.object({
 	id: z.string(),
@@ -108,5 +109,16 @@ export async function deleteInvoice(id: string) {
 	} catch (error) {
 		console.error('Database Error:', error);
 		return { message: 'Database Error: Failed to Delete Invoice.' };
+	}
+}
+
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+	try {
+		await signIn('credentials', Object.fromEntries(formData));
+	} catch (error) {
+		if ((error as Error).message.includes('CredentialsSignin')) {
+			return 'CredentialSignin';
+		}
+		throw error;
 	}
 }

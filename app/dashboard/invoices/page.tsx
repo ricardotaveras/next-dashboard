437da2ba0737cc11f3f8c89bc710fd330a2ec939
Ricard Ttaveras <ricardo.taveras@gmail.com>
2023-11-13 21:39:33 -0400

    Fix page dashborad

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -19,8 +19,6 @@ export default async function Page({
 	const currentPage = Number(searchParams?.page) || 1;
 	const totalPages = await fetchInvoicesPages(query);
 
-	const totalPages = await fetchInvoicesPages(query);
-
 	return (
 		<div className='w-full'>
 			<div className='flex w-full items-center justify-between'>
